ff0ecb943f1541bb66e22dcf4ba522ea9d0820b4
vlj2007 <vlj2007@yandex.ru>
2023-08-21 00:44:57 +0700

    I added toString and equals and hashCode.

--- src/pro/sky/java/course1/lesson2/Author.java
+++ src/pro/sky/java/course1/lesson2/Author.java
@@ -1,5 +1,5 @@
 package pro.sky.java.course1.lesson2;
-
+import java.util.Objects;
 public class Author {
     private String name;
     private String surname;
@@ -24,5 +24,22 @@ public class Author {
 
     public void getSurname(String surname) {
         this.surname = surname;
+    }
+
+    public String toString(){
+        return getName() + " " + getSurname();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
     }
 }
--- src/pro/sky/java/course1/lesson2/Book.java
+++ src/pro/sky/java/course1/lesson2/Book.java
@@ -1,5 +1,5 @@
 package pro.sky.java.course1.lesson2;
-
+import java.util.Objects;
 public class Book {
     private String bookTitle;
     private Author bookAuthor;
@@ -29,5 +29,23 @@ public class Book {
 
     public void setBookPublicationYear(int bookPublicationYear) {
         this.bookPublicationYear = bookPublicationYear;
+    }
+
+    public String toString() {
+        return "\tНазвание книги \"" + this.bookTitle + "\"\n\tавтор книги \"" + bookAuthor + "\"\n\tдата публикации " + this.bookPublicationYear;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return bookPublicationYear == book.bookPublicationYear && Objects.equals(bookTitle, book.bookTitle) && Objects.equals(bookAuthor, book.bookAuthor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookTitle, bookAuthor, bookPublicationYear);
     }
 }
